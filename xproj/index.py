b101edb25aca73cb5ff305d91f052d1be1b55cbd
Benoit Bovy <benbovy@gmail.com>
2024-12-03 15:31:45 +0100

    remove support of CRSIndex with no CRS defined
    This adds unnecessary complexity. Best way to "unset" the CRS is
    probably to just drop the coordinate or the index from the Dataset or
    DataArray.
    
    (maybe we'll circle back to this later).

--- xproj/index.py
+++ xproj/index.py
@@ -7,34 +7,27 @@ import xarray as xr
 from xarray.indexes import Index
 
 
-def _format_crs(crs: pyproj.CRS | None, max_width: int = 20) -> str:
-    if crs is not None:
-        srs = crs.to_string()
-    else:
-        srs = "None"
-
+def _format_crs(crs: pyproj.CRS, max_width: int = 20) -> str:
+    srs = crs.to_string()
     return srs if len(srs) <= max_width else " ".join([srs[:max_width], "..."])
 
 
 class CRSIndex(Index):
 
-    _crs: pyproj.CRS | None
+    _crs: pyproj.CRS
     _coord_names: list[Hashable]
 
-    def __init__(self, crs: pyproj.CRS | None = None):
-        if crs is not None:
-            crs = pyproj.CRS.from_user_input(crs)
-
-        self._crs = crs
+    def __init__(self, crs: pyproj.CRS):
+        self._crs = pyproj.CRS.from_user_input(crs)
         self._coord_names = []
 
     @property
-    def crs(self) -> pyproj.CRS | None:
+    def crs(self) -> pyproj.CRS:
         return self._crs
 
     @property
-    def coord_names(self) -> list[Hashable]:
-        return self.coord_names
+    def coord_names(self) -> tuple[Hashable, ...]:
+        return tuple(self._coord_names)
 
     @classmethod
     def from_variables(
@@ -52,30 +45,18 @@ class CRSIndex(Index):
             raise ValueError("can only create a CRSIndex from one scalar variable")
 
         # TODO: how to deal with different CRS in var attribute vs. build option?
-        crs = var.attrs.get("spatial_ref", options.get("crs"))
+        crs = var.attrs.get("spatial_ref", options["crs"])
 
-        return cls(crs=crs)
-
-    def _check_crs(self, other_crs: pyproj.CRS | None, allow_none: bool = False) -> bool:
-        """Check if the index's projection is the same than the given one.
-        If allow_none is True, empty CRS is treated as the same.
-        """
-        if allow_none:
-            if self.crs is None or other_crs is None:
-                return True
-        if not self.crs == other_crs:
-            return False
-        return True
+        return cls(crs)
 
     def equals(self, other: CRSIndex) -> bool:
         if not isinstance(other, CRSIndex):
             return False
-        if not self._check_crs(other.crs, allow_none=True):
+        if not self.crs == other.crs:
             return False
         return True
 
     def _repr_inline_(self, max_width: int) -> str:
-        # TODO: remove when fixed in XArray
         if max_width is None:
             max_width = xr.get_options()["display_width"]
 
